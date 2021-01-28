44adc7f8af7ddc4aa8183e47f6f89ea9edbfcf2a
johnny <johnny@johnnys-MacBook-Pro.local>
2021-01-28 22:58:56 +0800

    add some tools,tmux , git ,usbtools

--- xfce_exec/ubuntu/install/tools.sh
+++ xfce_exec/ubuntu/install/tools.sh
@@ -4,8 +4,23 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update 
-apt-get install -y vim wget net-tools locales bzip2 \
-    python-numpy #used for websockify/novnc
+apt-get install -y \
+    tmux \
+    vim \
+    wget \
+    git \
+    gedit \
+    net-tools \
+    locales \
+    bzip2 \
+    unzip \
+    openssh-client \
+    apt-utils \
+    usbutils \
+    python-pip \
+    python-dev\
+    ffmpeg \
+    python-numpy  #used for websockify/novnc
 apt-get clean -y
 
 echo "generate locales für en_US.UTF-8"
